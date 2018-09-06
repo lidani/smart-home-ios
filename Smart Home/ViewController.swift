f5cdb82e15c2306f7e5ed6a899b1e075fede3331
Gustavo Lidani <gustavo@mac-mini-de-hewerton.local>
2018-09-06 11:44:16 -0300

    Initial Commit

---
+++ Smart Home/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Smart Home
+//
+//  Created by Gustavo Lidani on 06/09/2018.
+//  Copyright © 2018 Gustavo Lidani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
